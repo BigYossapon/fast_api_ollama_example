a58d8e6f8bfbebfd6b31f9ed5e677891247c134d
BigYossapon <bigyossapon.forwork@gmail.com>
2025-03-20 00:22:33 +0700

    Initial commit

---
+++ llm_api_ollama.py
@@ -0,0 +1,40 @@
+from ollama import chat
+from ollama import ChatResponse
+from fastapi import FastAPI, Request, HTTPException
+from typing import Union
+
+from pydantic import BaseModel
+
+# 1.setup ollama
+# 2.ollama server on cmd
+# 3.go to web ollama select some model for your server
+# 4.get command to get model set up to cmd
+# 5.ollama list to check model have on server
+# 6.use this python code to run with fastapi dev name_file.py
+
+#อย่าลืมดูว่า model ที่ใช้หนักขนาดไหน ไม่ใช่ทุกโมเดลที่ คอมธรรมดาจะรันไหว
+
+app = FastAPI()
+
+class Ask(BaseModel):
+    question: str = ""
+    
+   
+# request:Request
+@app.post("/ask")
+async def ask_question(Ask:Ask):
+    
+
+   
+
+    response: ChatResponse = chat(model='llama3.2', messages=[
+    {
+        'role': 'user',
+        'content': Ask.question,
+    },
+    ])
+    print(response['message']['content'])
+    # or access fields directly from the response object
+    print(response.message.content)
+    return response.message.content
+
